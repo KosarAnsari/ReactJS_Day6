2beb9916152aa394e5d82fb314b3d00506c6d63c
KosarAnsari <kosaransari55@gmail.com>
2025-07-28 23:13:26 +0530

    Create College.jsx

---
+++ College.jsx
@@ -0,0 +1,12 @@
+function College({ names }) {
+  //console.log(props);
+  return (
+    <>
+      <h2>College Component</h2>
+      <p>{names[0]}</p>
+      <p>{names[1]}</p>
+      <p>{names[4]}</p>
+    </>
+  );
+}
+export default College;
